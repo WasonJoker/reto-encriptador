a7ffa21a5190b63cf0d87d3366e4c87289489e31
WasonJoker <polo.sustancial-00@icloud.com>
2024-08-11 10:33:24 -0600

    Se agrega codigo JS

---
+++ script.js
@@ -0,0 +1,23 @@
+const textArea = document.querySelector(".text-area")
+const mensaje = document.querySelector(".mensaje")
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "ines"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for (let i= 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.remplaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+
+    }
+    return stringEncriptado
+}
