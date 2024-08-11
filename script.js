a8dd4062742803eed8c572cb3a7eb4d142f2239c
WasonJoker <polo.sustancial-00@icloud.com>
2024-08-11 10:59:18 -0600

    Ya encripta, pero no desencripta xd

--- script.js
+++ script.js
@@ -7,17 +7,42 @@ const mensaje = document.querySelector(".mensaje")
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
-
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
 
 function encriptar(stringEncriptado){
     let matrizCodigo = [["e", "enter"], ["i", "ines"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
     stringEncriptado = stringEncriptado.toLowerCase()
 
-    for (let i= 0; i < matrizCodigo.length; i++) {
+    for (let i = 0; i < matrizCodigo.length; i++) {
         if(stringEncriptado.includes(matrizCodigo[i][0])) {
-            stringEncriptado = stringEncriptado.remplaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
         }
 
     }
     return stringEncriptado
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "ines"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencriptado
 }
